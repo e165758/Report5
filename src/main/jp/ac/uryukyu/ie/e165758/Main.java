8c3956b17b50a6da60115b77dedf0d6b16cc8d83
Okuto Matsuda <e165758@ie.u-ryukyu.ac.jp>
2016-12-11 21:07:01 +0900

    NullPointerExceptionを確認

---
+++ src/main/jp/ac/uryukyu/ie/e165758/Main.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e165758;
+
+/**
+ * Created by e165758 on 2016/12/11.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        int n = str.length();
+    }
+}
