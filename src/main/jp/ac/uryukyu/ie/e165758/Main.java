3a6b7178d51e931ffd14a23634f38371f9e89ffe
Okuto Matsuda <e165758@ie.u-ryukyu.ac.jp>
2016-12-11 21:52:43 +0900

    例外処理を追加

--- src/main/jp/ac/uryukyu/ie/e165758/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165758/Main.java
@@ -1,11 +1,16 @@
 package jp.ac.uryukyu.ie.e165758;
 
-/**
- * Created by e165758 on 2016/12/11.
- */
 public class Main {
-    public static void main(String[] args){
-        String str = null;
-        int n = str.length();
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            int n = str.length();
+        //    System.out.printf("strの長さは%d\n", n);
+        } catch (NullPointerException e) {
+                System.out.println("Error:NullPointerException");
+                System.out.println("\"str\"に\"Null\"が代入されています。");
+                System.out.println("以下スタックトレース");
+                e.printStackTrace();
+        }
     }
 }
