12f04189775141c3d1ebb03f331a305720e62dcb
Okuto Matsuda <e165758@ie.u-ryukyu.ac.jp>
2016-12-12 23:48:04 +0900

    value に 3．14 を代入した。

--- src/main/jp/ac/uryukyu/ie/e165758/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165758/Main.java
@@ -3,9 +3,12 @@ package jp.ac.uryukyu.ie.e165758;
 public class Main {
     public static void main(String[] args) {
         try {
-            String str = null;
-            int n = str.length();
-        //    System.out.printf("strの長さは%d\n", n);
+         //   String str = null;
+         //   int n = str.length();
+         //   System.out.printf("strの長さは%d\n", n);
+            String str = "3．14";
+            double value = Double.parseDouble(str);
+            System.out.println(value);
         } catch (NullPointerException e) {
                 System.out.println("Error:NullPointerException");
                 System.out.println("\"str\"に\"Null\"が代入されています。");
